cb68dac1c73d4d98c7fa2136aca87e942e3b8b1c
victorjiang <victorjiang189@163.com>
2025-01-12 23:19:07 +0800

    feat: 增加env.config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
 import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), //从默认位置加载并解析.env文件
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(3306),
+      }),
+    }), //从默认位置加载并解析.env文件
     CatModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
