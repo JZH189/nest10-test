a6ae6a6c61ab2f5eb9f0aebd0ded5e2e18a3ed7e
victorjiang <victorjiang189@163.com>
2025-01-15 23:47:23 +0800

    feat: 全局守卫

---
+++ src/common/guards/api-key/api-key.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Authorization');
+    console.log('authHeader: ', authHeader);
+    //做一些逻辑处理用来决定是否放行请求
+    return true;
+  }
+}
