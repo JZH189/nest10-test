e3d8815c771ae9810d3c7a571941384b1dac3e76
victorjiang <victorjiang189@163.com>
2025-01-16 22:16:24 +0800

    feat: 全局拦截器WrapResponseInterceptor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
       },
     }),
   );
+  //全局拦截器
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   //全局过滤器
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
