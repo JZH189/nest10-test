8bd928bacfa117f025dce6416bf968a3ece6b359
victorjiang <victorjiang189@163.com>
2025-01-17 00:13:11 +0800

    feat: 使用nest-knife4j开发api文档

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { knife4jSetup } from 'nest-knife4j';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
@@ -23,6 +25,25 @@ async function bootstrap() {
   );
   //全局过滤器
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  //http://127.0.0.1:3000/doc.html
+  knife4jSetup(app, [
+    {
+      name: '0.0.1版本',
+      url: `/api-json`,
+      swaggerVersion: '2.0',
+      location: `/api-json`,
+    },
+  ]);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
