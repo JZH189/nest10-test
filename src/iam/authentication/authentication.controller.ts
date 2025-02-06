57a93e874dbeeba2e51fc3e2a63dca0f345023b4
victorjiang <victorjiang189@163.com>
2025-02-06 23:51:22 +0800

    feat: 用户注册登录功能模拟

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -2,16 +2,24 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('验证模块')
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
 
+  @ApiOperation({
+    summary: '注册',
+  })
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
 
+  @ApiOperation({
+    summary: '登录',
+  })
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto) {
